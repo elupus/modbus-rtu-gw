ddca180cb07efb7a5c1f4c67ec6d07da4c5359ad
Joakim Plate <elupus@ecce.se>
2017-01-21 13:37:10 +0100

    Add lgpl license header

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright Joakim Plate <joakim.plate@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
 #include <stdio.h>
 int main(int argc, char** argv) {
       printf("hello world!\n");
